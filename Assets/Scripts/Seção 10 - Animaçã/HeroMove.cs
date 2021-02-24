490edc3594400b45c6d669b5ba0411565dcc4bde
Marlus Vinicius <marlusvinicius2014@outlook.com>
2021-02-24 16:21:50 -0300

    Adicionado script de controle e animações do zumbi

--- Assets/Scripts/Seção 10 - Animaçã/HeroMove.cs
+++ Assets/Scripts/Seção 10 - Animaçã/HeroMove.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class HeroMove : MonoBehaviour
 {
-    [SerializeField] private float speed = 2.5f;
+    [SerializeField] private float walkingSpeed = 2.5f;
+    [SerializeField] private float runningSpeed = 10f;
     [SerializeField] private float jumpForce = 6.5f;
+    // [SerializeField] private float boostTimeLimit = 1.0f;
 
     private bool _isFacingRight = true;
     private bool _canJump = false;
+    private bool _isAlive = true;
+    private bool _isRunning = false;
+    private bool _isShooting = false;
+    // private bool _timeIsRunning = false;
+    private float _speed;
     private float _horVelocity;
+    private float _direction;
+    // private float _boostTimer = 0f;
 
+    private Animator _anim;
     private Rigidbody2D _rb;
-    
+
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -27,16 +39,72 @@ public class HeroMove : MonoBehaviour
         // {
         //     Flip();
         // }
-        
-        if (((Input.GetAxisRaw("Horizontal") == 1) && !_isFacingRight) 
-            ||  (Input.GetAxisRaw("Horizontal") == -1 && _isFacingRight))
-            Flip();
 
-        _horVelocity = speed * Time.deltaTime * Input.GetAxisRaw("Horizontal");
-        transform.Translate(new Vector2(_horVelocity, 0));
+        if (_isAlive)
+        {
+            _direction = Input.GetAxisRaw("Horizontal");
+            
+            //Flip sprite
+            if ((_direction > 0 && !_isFacingRight) || (_direction < 0 && _isFacingRight))
+                Flip();
+            
+            //Walk to Idle and vice-versa
+            if (Input.GetButton("Horizontal"))
+            {
+                if (Input.GetKey(KeyCode.LeftShift)  && !_isShooting)
+                {
+                    if (_canJump)
+                        SetAnimParameter_X("isRunning");
+                    else
+                    {
+                        SetAnimParameter_X("isWalking");
+                    }
+                    
+                    _speed = runningSpeed;
+                }
+                else
+                {
+                    SetAnimParameter_X("isWalking");
+                    _speed = walkingSpeed;
+                }
 
-        if (Input.GetKeyDown(KeyCode.Space) && _canJump)
-            _rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+            else
+            {
+                SetAnimParameter_X("isIdle");
+            }
+
+            //Alter velocity
+            _horVelocity = _speed * Time.deltaTime * _direction;
+            transform.Translate(new Vector2(_horVelocity, 0));
+            
+            //Shoot
+            if (_canJump && !_isRunning && Input.GetKey(KeyCode.Z))
+            {
+                SetAnimParameter_Y("isShooting");
+            }
+            else
+            {
+                _anim.SetBool("isShooting", false);
+            }
+            
+            //Jump
+            if (Input.GetKeyDown(KeyCode.Space) && _canJump)
+            {
+                SetAnimParameter_Y("isJumping");
+                _rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+            
+            if (_rb.velocity.y > 0)
+                SetAnimParameter_Y("isJumping");
+            else if(_rb.velocity.y < 0)
+                SetAnimParameter_Y("isFalling");
+
+
+            
+
+
+        }
     }
 
     void Flip()
@@ -49,13 +117,86 @@ public class HeroMove : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("floor"))
+        if (other.gameObject.CompareTag("projectile"))
+        {
+            _isAlive = false;
+            _anim.SetBool("isDying", !_isAlive);
+        }
+        else if (other.gameObject.CompareTag("floor"))
             _canJump = true;
+        SetAnimParameter_Y("None");
     }
-    
+
     private void OnCollisionExit2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("floor"))
             _canJump = false;
     }
+
+    void SetAnimParameter_X(string parameter)
+    {
+        switch (parameter)
+        {
+            case "isIdle":
+                _anim.SetBool("isWalking", false);
+                _anim.SetBool("isRunning", false);
+                _anim.SetBool("isIdle", true);
+                _isRunning = false;
+                break;
+            case "isWalking":
+                _anim.SetBool("isWalking", true);
+                _anim.SetBool("isRunning", false);
+                _anim.SetBool("isIdle", false);
+                _isRunning = false;
+                break;
+            case "isRunning":
+                _anim.SetBool("isWalking", false);
+                _anim.SetBool("isRunning", true);
+                _anim.SetBool("isIdle", false);
+                _isRunning = true;
+                break;
+        }
+    }
+
+    void SetAnimParameter_Y(string parameter)
+    {
+        switch (parameter)
+        {
+            case "isShooting":
+                _anim.SetBool("isShooting", true);
+                _anim.SetBool("isJumping", false);
+                _anim.SetBool("isFalling", false);
+                break;
+            case "isJumping":
+                _anim.SetBool("isShooting", false);
+                _anim.SetBool("isJumping", true);
+                _anim.SetBool("isFalling", false);
+                break;
+            case "isFalling":
+                _anim.SetBool("isShooting", false);
+                _anim.SetBool("isJumping", false);
+                _anim.SetBool("isFalling", true);
+                break;
+            default:
+                _anim.SetBool("isShooting", false);
+                _anim.SetBool("isJumping", false);
+                _anim.SetBool("isFalling", false);
+                break;
+        }
+
+        _isShooting = _anim.GetBool("isShooting");
+
+    }
+
+    // void SetRunningTimer()
+    // {
+    //     if (_boostTimer < boostTimeLimit && _timeIsRunning)
+    //         _boostTimer += Time.deltaTime;
+    //     else
+    //     {
+    //         _timeIsRunning = false;
+    //         _boostTimer = 0;
+    //     }
+    // }
 }
+
---
+++ Assets/Scripts/Seção 10 - Animaçã/ZombieMove.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMove : MonoBehaviour
+{
+
+    [SerializeField] private Transform hero;
+    
+    [SerializeField] private float speed = 1.0f;
+
+   
+    
+    private bool _isChasing = false;
+    private float _dist;
+    private bool _isFacingLeft = true;
+
+    private Animator _anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        _dist = Vector2.Distance(transform.position, hero.position);
+
+        if ((hero.transform.position.x > transform.position.x) && _isFacingLeft)
+            Flip();
+        else if (hero.transform.position.x < transform.position.x && !_isFacingLeft)
+            Flip();
+
+        if (_isChasing && _dist > 2.8f)
+        {
+            if (hero.transform.position.x < transform.position.x)
+            {
+                transform.Translate(new Vector2(-speed * Time.deltaTime, 0));
+                _anim.SetBool("isWalking", true);
+                _anim.SetBool("isIdle", false);
+            }
+            else if (hero.transform.position.x > transform.position.x)
+            {
+                transform.Translate(new Vector2(speed * Time.deltaTime, 0));
+                _anim.SetBool("isWalking", true);
+                _anim.SetBool("isIdle", false);
+            }
+        }
+        else
+        {
+            _anim.SetBool("isWalking", false);
+            _anim.SetBool("isIdle", true);
+        }
+    }
+    
+    void Flip()
+    {
+        _isFacingLeft = !_isFacingLeft;
+        Vector3 reflection = transform.localScale;
+        reflection.x *= -1;
+        transform.localScale = reflection;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _isChasing = true;
+        }
+    }
+}
