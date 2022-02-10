814ea63ded289e06c3f178e6d8d1fe0dd7652a33
Marlus Vinicius <marlusvinicius2014@outlook.com>
2022-02-10 20:39:46 -0300

    Section 12 - Particle System - Fireworks Exercise - Unfinished

--- Assets/Scripts/Seção 12 - Sistemas de Partículas/ParticleManipulator.cs
+++ Assets/Scripts/Seção 12 - Sistemas de Partículas/ParticleManipulator.cs
@@ -5,8 +5,16 @@ namespace DefaultNamespace.Seção_12___Sistemas_de_Partículas
 {
     public class ParticleManipulator : MonoBehaviour
     {
+        [SerializeField] private Gradient lifetimeColor;
         private MainMovement _mainMovement;
         private ParticleSystem _particleSystem;
+
+        #region ParticleSystemModules
+
+        private ParticleSystem.ExternalForcesModule _externalForces;
+        private ParticleSystem.NoiseModule _noise;
+
+        #endregion
 
         private void Awake()
         {
@@ -14,15 +22,34 @@ namespace DefaultNamespace.Seção_12___Sistemas_de_Partículas
         }
 
 
+        private void Update()
+        {
+            if (_particleSystem != null)
+            {
+                _externalForces.multiplier = _mainMovement.Rb.velocity.x;
+                // ParticleSystem.MinMaxCurve curve = new ParticleSystem.MinMaxCurve();
+                // curve.constantMin = 
+                _noise.strength = _mainMovement.Rb.velocity.y;
+            }
+        }
+
         private void OnTriggerEnter2D(Collider2D other)
         {
-            Debug.Log(other.name);
-
             if (other.transform.parent == null) return;
             if (other.transform.parent.TryGetComponent(out _particleSystem))
             {
+                GetParticleSystemModules();
                 _particleSystem.Play();
             }        
+        }
+
+        private void GetParticleSystemModules()
+        {
+            if (_particleSystem != null)
+            {
+                _externalForces = _particleSystem.externalForces;
+                _noise = _particleSystem.noise;
+            }
         }
     }
 }
