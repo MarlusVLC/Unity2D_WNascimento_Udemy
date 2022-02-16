ada91999532889db9f085df17ddcdf05345d0dd4
Marlus Vinicius <marlusvinicius2014@outlook.com>
2022-02-15 23:20:08 -0300

    Section 14 - Clearing doubts - Animation details - Unfinished

--- Assets/Scripts/Movement/HeroMovement_William.cs
+++ Assets/Scripts/Movement/HeroMovement_William.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HeroMovement_William : MonoBehaviour
+namespace Movement
 {
-    // Start is called before the first frame update
-    void Start()
+    public class HeroMovement_William : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Rigidbody2D heroiRB;
+        [SerializeField] private float maxSpeed = 5f;
+        [Header("UI")] 
+        [SerializeField] private Text txtMagnet; 
 
-    // Update is called once per frame
-    void Update()
-    {
+        private bool face = true;
+
+        private Animator anim;
+
+        private bool noChao;
+        private Transform noChaoCheck;
+        private float noChaoRaio = 0.2f;
+        private float jumpForce = 1000f;
+        private float move;
+        private int magnetItem = 0;
+
+        void Start()
+        {
+            anim = GetComponent<Animator>();
+        }
+        
         
     }
 }
+
