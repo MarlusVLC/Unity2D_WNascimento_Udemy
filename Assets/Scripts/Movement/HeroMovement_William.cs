66897f4d19dff4ec303f57622a61d12858ea393d
Marlus Vinicius <marlusvinicius2014@outlook.com>
2022-02-16 07:58:26 -0300

    Section 14 - Clearing doubts - Animation details - Sample done

--- Assets/Scripts/Movement/HeroMovement_William.cs
+++ Assets/Scripts/Movement/HeroMovement_William.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,30 +6,115 @@ using UnityEngine.UI;
 
 namespace Movement
 {
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Collider2D))]
     public class HeroMovement_William : MonoBehaviour
     {
-        [SerializeField] private Rigidbody2D heroiRB;
+        [Header("Movement")]
         [SerializeField] private float maxSpeed = 5f;
+        [SerializeField] private float jumpForce = 1000f;
+        
         [Header("UI")] 
-        [SerializeField] private Text txtMagnet; 
+        [SerializeField] private Text txtMagnet;
+
+        [Header("Ground detection")] 
+        [SerializeField] private LayerMask oQueEChao;
+        [SerializeField] private Transform noChaoCheck;
+        [SerializeField] private float noChaoRaio = 0.2f;
 
         private bool face = true;
-
-        private Animator anim;
-
         private bool noChao;
-        private Transform noChaoCheck;
-        private float noChaoRaio = 0.2f;
-        private float jumpForce = 1000f;
         private float move;
         private int magnetItem = 0;
 
+        private Animator anim;
+        private Rigidbody2D heroiRB;
+
+
         void Start()
         {
+            heroiRB = GetComponent<Rigidbody2D>();
             anim = GetComponent<Animator>();
         }
-        
-        
+
+        private void FixedUpdate()
+        {
+            noChao = Physics2D.OverlapCircle(noChaoCheck.position, noChaoRaio, oQueEChao);
+            
+            //Idle
+            anim.SetBool("Idle", noChao);
+            
+            //Pulo
+            anim.SetBool("Jump", !noChao);
+
+            move = Input.GetAxis("Horizontal");
+            
+            anim.SetFloat("Walk", Mathf.Abs(move));
+
+            heroiRB.velocity = new Vector2(move * maxSpeed, heroiRB.velocity.y);
+            
+            ///Tiro andando
+            float tiroParado = Input.GetAxis("Fire1");
+
+            if (tiroParado >= 1 && Mathf.Abs(move) > 0.1f)
+            {
+                anim.SetBool("WalkNShot", true);
+                anim.SetBool("StopShot", false);
+            }
+            else if (tiroParado >= 1 && Mathf.Abs(move) < 0.01f)
+            {
+                anim.SetBool("WalkNShot", false);
+                anim.SetBool("StopShot", true);
+            }
+            
+            //Tiro parado
+            else if (tiroParado >= 1)
+            {
+                anim.SetBool("StopShot", true);
+            }
+            else if (tiroParado <= 0)
+            {
+                anim.SetBool("StopShot", false);
+            }
+            
+            ////////////////////////////////////////////////////////////////
+
+            if (move > 0 && !face)
+            {
+                Flip ();
+            }
+            else if (move < 0 && face)
+            {
+                Flip ();
+            }
+        }
+
+        private void Update()
+        {
+            if (noChao && Input.GetKeyDown(KeyCode.Space))
+            {
+                anim.SetBool("Idle", false);
+                heroiRB.AddForce(new Vector2(0, jumpForce));
+            }
+        }
+
+        void Flip()
+        {
+            face = !face;
+            Vector3 tempScale = transform.localScale;
+            tempScale.x *= -1;
+            transform.localScale = tempScale;
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("coin"))
+            {
+                magnetItem++;
+                if (txtMagnet != null) txtMagnet.text = magnetItem.ToString();
+                Destroy(other.gameObject);
+            }
+        }
     }
 }
 
